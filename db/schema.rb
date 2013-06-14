73d4f6f25c5a597ccf6c472d97097144ef2da94e
jerome <jerome@localhost.localdomain>
2013-06-14 10:03:55 +0800

    添加了gemfile.lock,*.sqlite3,development.log到git忽略

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130610080931) do
+ActiveRecord::Schema.define(:version => 20130613034258) do
+
+  create_table "courses", :force => true do |t|
+    t.string   "name"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
 
   create_table "teachers", :force => true do |t|
     t.string   "last_name"
@@ -35,6 +41,7 @@ ActiveRecord::Schema.define(:version => 20130610080931) do
     t.datetime "created_at",           :null => false
     t.datetime "updated_at",           :null => false
   end
+
   create_table "users", :force => true do |t|
     t.string   "first_name"
     t.string   "last_name"
@@ -47,4 +54,5 @@ ActiveRecord::Schema.define(:version => 20130610080931) do
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
   add_index "users", ["remember_token"], :name => "index_users_on_remember_token"
+
 end
