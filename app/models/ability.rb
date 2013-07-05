1120b3799af7814cac887ef5e4b6f4c14f0cbd17
Jerome <canni3269@gmail.com>
2013-07-05 14:52:10 +0800

    add access control with teacher, user

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -4,12 +4,39 @@ class Ability
   def initialize(user)
     # Define abilities for the passed in user here. For example:
     #
+    alias_action :update, :destroy, :to => :modify
+    alias_action :create, :read, :update, :destroy, :to => :crud
+    puts 'current user ----'+user.class.to_s
+    if user.class.to_s == 'User'
       user ||= User.new # guest user (not logged in)
       if user.admin?
         can :manage, :all
       else
         can :read, :all
       end
+      can :read, Course
+      can [:new, :create, :myclasses], User
+      can :manage, User,  :id => user.id
+      can [:index, :show, :apply], Clclass
+      cannot :manage, Teacher
+      
+    elsif user.class.to_s == 'Teacher'
+      user ||= Teacher.new # guest user (not logged in)
+      
+      can :manage, Teacher
+      if user.admin?
+        can :manage, :all
+      else
+        can :read, :all
+      end
+      can :read, Course
+      can [:new, :create, :myclasses], User
+      can :manage, User,  :id => user.id
+      can [:index, :show, :apply], Clclass
+      
+    end
+    
+    
     #
     # The first argument to `can` is the action you are giving the user 
     # permission to do.
